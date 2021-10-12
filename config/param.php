c87094633d48bc986b74af1bb0c1f264302fe8ba
Bellusfennec <bellusfennec@gmail.com>
2021-10-12 20:55:05 +0300

    fix 2

--- config/param.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'admin_email' => 'bellusfennec@gmail.com',
-    'shop_name' => 'Интернет магазин ZERDA',
-    'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
-];
--- public_html/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once '../core/redirect.php';
-include_once 'home.html';
-?>
-!
-
