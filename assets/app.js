5a45f0da3a6e55796e959cc6ae32f55710a68ff5
Bellusfennec <bellusfennec@gmail.com>
2021-12-10 21:47:45 +0300

    добавлены иконки

--- assets/app.js
+++ assets/app.js
@@ -206,7 +206,8 @@ function getDeck() {
     const deck = []
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
     // const suits = ["clubs", "diamonds", "hearts", "spades"]
-    const suits = ["&clubs;&#xFE0E;", "&diams;&#xFE0E;", "&hearts;&#xFE0E;", "&spades;&#xFE0E;"]
+    // const suits = ["&clubs;&#xFE0E;", "&diams;&#xFE0E;", "&hearts;&#xFE0E;", "&spades;&#xFE0E;"]
+    const suits = ["&#xe918;", "&#xe919;", "&#xe9da;", "&#xe917;"]
     var types = new Array("&clubs;", "&diams;", "&hearts;", "&spades;");
 
     suits.forEach(function(suit) {
