eb363abb58234ccebb6422585f929629a365755d
Bellusfennec <bellusfennec@gmail.com>
2022-01-06 21:25:04 +0300

    23

--- app.js
+++ app.js
@@ -7,28 +7,20 @@ const mysql = require("mysql2")
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     database: "bellus0a_zerda",
-//     password: "&lQyPzx5"
-//   });
-//   connection.connect(function(err){
-//     if (err) {
-//       return console.error("Ошибка: " + err.message);
-//     }
-//     else{
-//       console.log("Подключение к серверу MySQL успешно установлено");
-//     }
-//  })
-
-
-
-
-
-
-
-
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "bellus0a_zerda",
+    password: "&lQyPzx5"
+  });
+  connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ })
 
 const CONTACTS = [
     {id: v4(), name: 'Нияз', value: '891700022200', marked: false}
@@ -42,8 +34,9 @@ app.get('/api/contacts', (req, res) => {
 
 app.post('/api/contacts', (req, res) => {
 
-    // console.log(req.body)
+    console.log(req.body)
     const contact = {...req.body, id: v4(), marked: false}
+    console.log(contacty)
     CONTACTS.push(contact)
     res.status(201).json(contact)
 })
@@ -55,14 +48,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
 
-app.listen(PORT, () => console.log('Server has been started...'))
-
-// const http = require('http');
-
-// let server = http.createServer(function(req, res) {
-// res.writeHead(200, { 'Content-Type': 'text/plain' });
-// res.end('Hello World!!');
-// });
-
-// server.listen(PORT, () => console.log('Server has been started...'))
+app.listen(PORT, () => console.log(`Server has been started... (port: ${PORT})`))
 
